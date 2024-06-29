fadc4148be19b117cd291a1f98a03f70e9a73453
Kyutashii <kyutashiixdd@gmail.com>
2024-06-29 21:53:17 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -16,7 +16,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
 
     if (!prefix) {
         api.sendMessage(
-            "I don't have a prefix set.",
+            "𝘀𝗼𝗿𝗿𝘆 𝗭𝗲𝗻𝗽𝗮𝗶 𝗜 𝗱𝗼𝗻'𝘁 𝗵𝗮𝘃𝗲 𝗮 𝗽𝗿𝗲𝗳𝗶𝘅 𝘀𝗲𝘁.(◍•ᴗ•◍)",
             threadID,
             messageID
         );
@@ -26,7 +26,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
     // Check if the command is invoked manually with the prefix
     if (body.toLowerCase() === `${prefix}prefix`) {
         api.sendMessage(
-            `Hey there! My prefix is [ 𓆩 ${prefix} 𓆪 ].`,
+            `𝗛𝗲𝘆𝘆 𝗭𝗲𝗻𝗽𝗮𝗶 𝘁𝗵𝗲𝗿𝗲! 𝗠𝘆 𝗽𝗿𝗲𝗳𝗶𝘅 𝗶𝘀 [  ${prefix}  ].`,
             threadID,
             messageID
         );
@@ -36,7 +36,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
     // Sending the message along with the attachment
     api.sendMessage(
         {
-            body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
+            body: `𝗛𝗲𝗹𝗹𝗼 𝗭𝗲𝗻𝗽𝗮𝗶, 𝗺𝘆 𝗽𝗿𝗲𝗳𝗶𝘅 𝗶𝘀 [ ${prefix}  ]\n▬▬▬▬▬▬▬▬▬▬▬▬\n[📚]-𝗛𝗼𝘄 𝗧𝗼 𝗨𝘀𝗲𝗱??\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\n▬▬▬▬▬▬▬▬▬▬▬▬\nBot Developer: https://www.facebook.com/itssmekylebaitit`,
             attachment: fs.createReadStream(__dirname + "/cache2/prefix.jpeg")
         },
         threadID,
@@ -53,7 +53,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
                 threadID,
                 () => {}
             );
-            api.setMessageReaction("🚀", messageInfo.messageID, (err) => {}, true);
+            api.setMessageReaction("🌐", messageInfo.messageID, (err) => {}, true);
         }
     );
 };
