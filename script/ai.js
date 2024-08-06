f2f6b1ae65ee99b0ac78a523f6c53e750deb6a29
Kyledev033 <kyledev40@gmail.com>
2024-08-06 10:46:53 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,32 +1,36 @@
 const axios = require('axios');
+const moment = require('moment-timezone');
+
 module.exports.config = {
   name: 'ai',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['gpt', 'openai'],
-  description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
-  credits: 'Kyle敦. ဗီူ',
+  aliases: ['snow', 'ai'],
+  description: "An AI command powered by Snowflakes AI",
+  usage: "snowflakes [prompt]",
+  credits: 'Kylepogi',//modified by joshua Apostol
   cooldown: 3,
 };
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
+ 
+module.exports.run = async function({ api, event, args }) {
   const input = args.join(' ');
+  const timeString = moment.tz('Asia/Manila').format('LLL');
+ 
   if (!input) {
-    api.sendMessage(`[🤖]—𝗔𝘂𝘁𝗼𝗟𝘂𝗯𝗼𝘁𝘃𝟯\n    （„• ֊ •„)♡\n▬▬▬▬▬▬▬▬▬▬▬▬\n\n How can I help you today? `, event.threadID, event.messageID);
+    api.sendMessage(`𝗔𝗜 𝗚𝗣𝗧\n\n💁🏻‍♂️ Please provide a question/query.`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(``, event.threadID, event.messageID);
+ 
+  api.sendMessage(`🔍𝘀𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴....`, event.threadID, event.messageID);
+ 
   try {
-    const {
-      data
-    } = await axios.get(`https://hercai.onrender.com/v3/hercai`);
-    const response = data.response;
-    api.sendMessage('[🤖]—𝗔𝘂𝘁𝗼𝗟𝘂𝗯𝗼𝘁𝘃𝟯\n    （„• ֊ •„)♡\n▬▬▬▬▬▬▬▬▬▬▬▬\n['+ response +']\n▬▬▬▬▬▬▬▬▬▬▬▬\n[📚]|𝗚𝗣𝗧-𝟰 ', event.threadID, event.messageID);
+    const { data } = await axios.get(`https://hashier-api-snowflake.vercel.app/api/snowflake?ask=${encodeURIComponent(input)}`);
+    if (data.response) {
+      api.sendMessage(`𝗔𝗜 𝗚𝗣𝗧\n━━━━━━━━━━━━━━━\n\n${data.response}\n\n${timeString}`, event.threadID, event.messageID);
+    } else {
+      api.sendMessage('No response found.', event.threadID, event.messageID);
+    }
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
