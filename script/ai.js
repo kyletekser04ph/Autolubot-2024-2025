20dc359f9b419dd98e7810b4f0fc3a5bfd4cf941
Kyledev033 <kyledev40@gmail.com>
2024-08-17 15:00:32 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,36 +1,32 @@
 const axios = require('axios');
-const moment = require('moment-timezone');
-
 module.exports.config = {
   name: 'ai',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['snow', 'ai'],
-  description: "An AI command powered by Snowflakes AI",
-  usage: "snowflakes [prompt]",
-  credits: 'Kylepogi',//modified by joshua Apostol
+  aliases: ['gpt', 'openai'],
+  description: "An AI command powered by GPT-4",
+  usage: "Ai [promot]",
+  credits: 'Developer',
   cooldown: 3,
 };
- 
-module.exports.run = async function({ api, event, args }) {
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
   const input = args.join(' ');
-  const timeString = moment.tz('Asia/Manila').format('LLL');
- 
   if (!input) {
-    api.sendMessage(`𝗔𝗜 𝗚𝗣𝗧\n\n💁🏻‍♂️ Please provide a question/query.`, event.threadID, event.messageID);
+    api.sendMessage(`Please provide a question or statement after 'ai'. For example: 'ai What is love? `, event.threadID, event.messageID);
     return;
   }
- 
-  api.sendMessage(`🔍𝘀𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴....`, event.threadID, event.messageID);
- 
+  api.sendMessage(`🔍 𝘼𝙄 is searching "${input}" please wait.`, event.threadID, event.messageID);
   try {
-    const { data } = await axios.get(`https://hashier-api-snowflake.vercel.app/api/snowflake?ask=${encodeURIComponent(input)}`);
-    if (data.response) {
-      api.sendMessage(`𝗔𝗜 𝗚𝗣𝗧\n━━━━━━━━━━━━━━━\n\n${data.response}\n\n${timeString}`, event.threadID, event.messageID);
-    } else {
-      api.sendMessage('No response found.', event.threadID, event.messageID);
-    }
+    const {
+      data
+    } = await axios.get(`https://soyeon-gpt4.onrender.com/api?prompt=${encodeURIComponent(input)}`);
+    const response = data.response;
+    api.sendMessage(response + '\n\n', event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
